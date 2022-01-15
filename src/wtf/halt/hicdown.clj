d0303892b8d29fcfc086aacc1eacaf6fee9be866
June Kelly <june@cnjctr.com>
2022-01-15 21:38:20 +0000

    Better main

--- src/wtf/halt/hicdown.clj
+++ src/wtf/halt/hicdown.clj
@@ -1,17 +1,14 @@
 (ns wtf.halt.hicdown
   (:require [instaparse.core :as insta]
-              [clojure.java.io :as io])
+            [clojure.java.io :as io]
+            [clojure.pprint :as pp])
   (:gen-class))
 
 (def parser
   (insta/parser (io/resource "hicdown.bnf")))
 
-(defn greet
-  "Callable entry point to the application."
-  [data]
-  (println (str "Hello, " (or (:name data) "World") "!")))
-
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (greet {:name (first args)}))
+  (-> (parser (first args))
+      (pp/pprint)))
