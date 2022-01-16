87ae35fa1458dca832a86d10846dd582d6b7d842
June Kelly <june@cnjctr.com>
2022-01-16 08:39:21 +0000

    Add basic test

--- test/wtf/halt/hicdown_test.clj
+++ test/wtf/halt/hicdown_test.clj
@@ -2,6 +2,7 @@
   (:require [clojure.test :refer :all]
             [wtf.halt.hicdown :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest simple-document
+  (testing "a simple document"
+    (is (= (parser "Hello")
+           [:Document [:Block [:TextContent "Hello"]]]))))
