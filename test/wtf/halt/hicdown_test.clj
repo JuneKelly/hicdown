4e05aad6c672c570032c0165b580a62cd0935ebf
June Kelly <june@cnjctr.com>
2022-01-16 09:00:29 +0000

    Better test harness

--- test/wtf/halt/hicdown_test.clj
+++ test/wtf/halt/hicdown_test.clj
@@ -1,8 +1,20 @@
 (ns wtf.halt.hicdown-test
   (:require [clojure.test :refer :all]
-            [wtf.halt.hicdown :refer :all]))
+            [clojure.java.io :as io]
+            [clojure.string :as string]
+            [wtf.halt.hicdown :as hicdown]))
+
+(defn test-file [path]
+  (-> (io/resource (str "test/" path))
+      (slurp)
+      (string/trim)))
+
+(defn parse-test-file [path]
+  (-> (test-file path)
+      (hicdown/parser)))
 
 (deftest simple-document
   (testing "a simple document"
-    (is (= (parser "Hello")
-           [:Document [:Block [:TextContent "Hello"]]]))))
+    (is (= (parse-test-file "simple.hd")
+           [:Document
+            [:Block [:TextContent "Hello"]]]))))
