da02a2a92139c405159486a297fc3b2ff3580c54
June Kelly <june@cnjctr.com>
2022-01-17 11:30:11 +0000

    Handle comments at block level

--- test/wtf/halt/hicdown_test.clj
+++ test/wtf/halt/hicdown_test.clj
@@ -212,4 +212,32 @@
           [:KVPair [:key ":bb"] [:Qval ~@(s "c  c ") [:valesc "\\\""] ~@(s " z z")]]
           [:KVPair [:key ":dd"] [:Qval ~@(s "e}e")]]]
          ~@(s " foo")]
-        ~@(s " bar.")]])))
+        ~@(s " bar.")]]))
+
+  (testing "Comments"
+
+    (test-parse
+     "comments/basic.hd"
+     `[:Document
+       [:comment ";; A note"]
+       [:Block
+        ~@(s "Test one ")
+        [:Segment [:tag ":a"]
+         [:Attrs [:KVPair [:key ":x"] [:val "y"]]]
+         ~@(s " foo")]
+        ~@(s " bar.")]
+       [:comment ";; Another note"]
+       [:Block
+        ~@(s "Hello.")]])
+
+    (test-parse
+     "comments/comment-mark-inside-blocks.hd"
+     `[:Document
+       [:comment ";; A note"]
+       [:Block
+        ~@(s "Some text ;; with semi-colons.")]
+       [:comment ";; Another note"]
+       [:Block
+        ~@(s ";; this doesn't count as a comment")
+        [:nl]
+        ~@(s "because it's actually inside a block.")]])))
