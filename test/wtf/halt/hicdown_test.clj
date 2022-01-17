9cee495b69ddd13170dc3410d2cf71a6bec6edc4
June Kelly <june@cnjctr.com>
2022-01-17 11:10:09 +0000

    Handle quoted attr values

--- test/wtf/halt/hicdown_test.clj
+++ test/wtf/halt/hicdown_test.clj
@@ -174,4 +174,42 @@
         [:esc "\\\\"]
         [:Segment [:tag ":a"]
          ~@(s "foo")]
+        ~@(s " bar.")]]))
+
+  (testing "Attrs"
+
+    (test-parse
+     "attrs/basic.hd"
+     `[:Document
+       [:Block
+        ~@(s "Test ")
+        [:Segment [:tag ":aa"]
+         [:Attrs
+          [:KVPair [:key ":bb"] [:val "cc"]]
+          [:KVPair [:key ":dd"] [:val "ee"]]]
+         ~@(s " foo")]
+        ~@(s " bar.")]])
+
+    (test-parse
+     "attrs/with-quoted-values.hd"
+     `[:Document
+       [:Block
+        ~@(s "Test ")
+        [:Segment [:tag ":aa"]
+         [:Attrs
+          [:KVPair [:key ":bb"] [:Qval ~@(s "c  c")]]
+          [:KVPair [:key ":dd"] [:Qval ~@(s "e}e")]]]
+         ~@(s " foo")]
+        ~@(s " bar.")]])
+
+    (test-parse
+     "attrs/with-quoted-values-and-escapes.hd"
+     `[:Document
+       [:Block
+        ~@(s "Test ")
+        [:Segment [:tag ":aa"]
+         [:Attrs
+          [:KVPair [:key ":bb"] [:Qval ~@(s "c  c ") [:valesc "\\\""] ~@(s " z z")]]
+          [:KVPair [:key ":dd"] [:Qval ~@(s "e}e")]]]
+         ~@(s " foo")]
         ~@(s " bar.")]])))
